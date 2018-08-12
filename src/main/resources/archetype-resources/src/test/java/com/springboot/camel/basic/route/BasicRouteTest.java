a59b89b5b0f76927fb027c78598aaa829697a9b8
Nihar Malali <blitznihar@gmail.com>
2018-08-12 18:06:39 -0500

    updating yaml and tests

--- src/main/resources/archetype-resources/src/test/java/com/springboot/camel/basic/route/BasicRouteTest.java
+++ src/main/resources/archetype-resources/src/test/java/com/springboot/camel/basic/route/BasicRouteTest.java
@@ -5,7 +5,7 @@ package com.springboot.camel.basic.route;
 
 import static org.junit.Assert.assertTrue;
 
-import org.apache.camel.Exchange;
+
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.test.spring.CamelSpringBootRunner;
 import org.junit.Test;
@@ -13,11 +13,22 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("dev")
+
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+
+@ActiveProfiles("unittest")
 @RunWith(CamelSpringBootRunner.class)
 @DirtiesContext(classMode= DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest
@@ -33,29 +44,58 @@ public class BasicRouteTest {
 	@Test
 	public void testMoveMQtoMQ() throws InterruptedException {
 		
-		String fileName = "testEmployeeSalaryProcessing.txt";
-		String routeFrom = "routeIn";
-		String message = "employeeId,rates${symbol_escape}n"+
-							"EM001, 140.00${symbol_escape}n"+
-							"EM016, 180.00${symbol_escape}n"+
-							"EM018, 150.00${symbol_escape}n"+
-							"EM031, 130.00${symbol_escape}n"+
-							"EM034, 120.00${symbol_escape}n";
-		producerTemplate.sendBodyAndHeader(environment.getProperty(routeFrom), message,Exchange.FILE_NAME,fileName);
-		Thread.sleep(3000);
-		System.out.println(message);
-		assertTrue(!message.isEmpty());
-		System.out.println(message);
+		String message = "employeeId,rates\n"+
+							"EM001, 140.00\n"+
+							"EM016, 180.00\n"+
+							"EM018, 150.00\n"+
+							"EM031, 130.00\n"+
+							"EM034, 120.00\n";
+		producerTemplate.sendBody("{{routeIn}}", message);
+		//Thread.sleep(10000);
+		
+		try {
+
+            // Create a ConnectionFactory
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
+        
+
+            // Create a Connection
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+
+            // Create a Session
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Create the destination (Topic or Queue)
+            Destination destination = session.createQueue("foo");
+
+            // Create a MessageConsumer from the Session to the Topic or Queue
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            // Wait for a message
+            Message messageO = consumer.receive(1000);
+
+            if (messageO instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) messageO;
+                String text = textMessage.getText();
+                messageOut=text;
+                System.out.println("Received: " + text);
+            } else {
+                System.out.println("Received: " + messageO);
+            }
+           
+            consumer.close();
+            session.close();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Caught: " + e);
+            e.printStackTrace();
+        }
+
+		System.out.println(messageOut);
+		assertTrue(!messageOut.isEmpty());
+		System.out.println(messageOut);
 	}
 	
-	@JmsListener(destination = "java2blog.queue")
-	public void receiveQueue(String text) {
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		messageOut = text;
-	}
 	
 }
